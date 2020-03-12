0c28c300d78e3431e2491ee7274f6da221ebf3f3
Emarin19 <emarin702740530@gmail.com>
2020-03-11 19:42:36 -0600

    replacce files

---
+++ Calculator/src/Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+    public double sum(double num1, double num2){
+        return num1+num2;
+    }
+
+    public double substraction(double num1, double num2){
+        return num1-num2;
+    }
+
+    public double multiplication(double num1, double num2){
+        return num1*num2;
+    }
+
+    public double division(double num1, double num2){
+        return num1/num2;
+    }
+}
