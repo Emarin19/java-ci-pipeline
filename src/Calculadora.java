b2ba7e2007fa01cb6cd8e229d3c9c6858fb5c392
Emarin19 <emarin702740530@gmail.com>
2020-03-13 00:23:55 -0600

    new method

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -14,4 +14,11 @@ public class Calculadora {
     public double division(double num1, double num2){
         return num1/num2;
     }
+
+    public double absolute(double num){
+        if(num<0){
+            return -num;
+        }
+        return num;
+    }
 }
